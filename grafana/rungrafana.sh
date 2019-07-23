1c61f5f64e4174405f3c672a53f28187c7a4c059
Ubuntu <denzilr@sqlmonitoring.ooalir02km0evhdlo5g1rrqdea.xx.internal.cloudapp.net>
2019-07-23 02:47:13 +0000

    Azure SQL DB Dashboard

---
+++ grafana/rungrafana.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#We use the grafana image that Grafana Labs provides http://docs.grafana.org/installation/docker/
+# If you wish to modify the port that Grafana runs on, you can do that here.
+GRAFANA_HOST_DIRECTORY="/data/grafana"
+
+sudo docker run --user root  --detach -p 3000:3000 --net=host --restart=always \
+	-v $GRAFANA_HOST_DIRECTORY:/var/lib/grafana \
+	-e "GF_INSTALL_PLUGINS=grafana-azure-monitor-datasource,grafana-piechart-panel,savantly-heatmap-panel" \
+	--name grafana grafana/grafana
+
+
+
---
+++ influxdb/runinfluxdb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Make sure this folder exists on the host.Ideally it is a volume reserved for influxdb
+# This directory from the host gets passed through to the docker container.
+INFLUXDB_HOST_DIRECTORY="/data/influxdb"
+
+
+sudo docker run --detach --net=host \
+	-v $INFLUXDB_HOST_DIRECTORY:/var/lib/influxdb:rw \
+	--hostname influxdb \
+	--restart=always \
+ 	-p 8086:8086 \
+        --name influxdb influxdb:latest
+
