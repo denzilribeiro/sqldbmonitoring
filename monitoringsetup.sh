9786912f67c1a70452c42cfd4f167862abbfbc1c
Ubuntu <denzilr@sqlmonitoring.ooalir02km0evhdlo5g1rrqdea.xx.internal.cloudapp.net>
2019-07-26 02:43:38 +0000

    updated setup and instructions

---
+++ monitoringsetup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "**********Starting Setup **********"
+dpkg-query -l docker.io
+
+if [ $? -eq 0 ]
+then
+  echo "***********Installing Docker***************"
+  sudo apt-get install docker.io -y
+else
+  echo "Docker already installed"	
+fi
+
+echo "************Installing Telegraf Nightly***********"
+cd $HOME
+wget https://dl.influxdata.com/telegraf/nightlies/telegraf_nightly_amd64.deb
+sudo dpkg -i telegraf_nightly_amd64.deb
+
+
+echo "********* Firewall rule for Grafana port **********"
+sudo ufw allow 3000/tcp
+sudo ufw reload
+
+echo "****** copying telegraf.conf sample for SQL, original conf renamed to /etc/telegraf_original.conf  ****/"
+sudo mv /etc/telegraf/telegraf.conf /etc/telegraf/telegraf_original.conf 
+sudo cp $HOME/sqlmimonitoring/telegraf/telegraf.conf /etc/telegraf/telegraf.conf 
+sudo chown root:root /etc/telegraf/telegraf.conf 
+sudo chmod 644 /etc/telegraf/telegraf.conf
+
+echo "********Pulling grafana containger ****************"
+cd $HOME/sqlmimonitoring/grafana
+sudo ./rungrafana.sh
+
+
